fc2bc94ea568fcc742b38b073f0a26c14ab7745f
NatalieGuseva <gusberd81@gmail.com>
2024-12-10 23:56:08 +0500

    Task executed

---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,38 @@
+package web.service;
+
+import org.springframework.stereotype.Component;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CarServiceImpl implements CarService {
+
+    private List<Car> cars;
+
+    {
+        cars = new ArrayList<>();
+        cars.add(new Car("bmw", 11, 1));
+        cars.add(new Car("mersedes", 12, 2));
+        cars.add(new Car("audi", 13, 3));
+        cars.add(new Car("Reno", 14, 4));
+        cars.add(new Car("Autovaz", 15, 5));
+    }
+
+
+    @Override
+    public List<Car> getCars(Integer count) {
+        List<Car> res = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            res.add(cars.get(i));
+        }
+        return res;
+    }
+
+    @Override
+    public List<Car> getAllCars() {
+        return cars;
+    }
+
+}
